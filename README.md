707daa18719def8581af67526282a8286b218bd4
Matt Dziuban <mrdziuban@gmail.com>
2016-02-18 09:13:50 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Chrome New Tab
+
+This is a template for a Chrome browser extension that overrides the default new tab. It replaces it with a page where you can add links to helpful resources. The extension also displays bookmarks from your Bookmarks Bar and will display all of your open GitHub pull requests.
+
+## Installation
+
+- Go to your [Personal access tokens settings in GitHub](https://github.com/settings/tokens) and generate a new token that can read all of the target organization's repositories. You will need this to set up the extension.
+- Install [khaos](khaos.io) for templating
+```bash
+$ npm install -g khaos
+```
+- Install this repo as a khaos template
+```bash
+$ khaos install mrdziuban/chrome-new-tab chrome-new-tab
+```
+- Run the templating engine
+```bash
+$ khaos create chrome-new-tab <your-project-name>
+```
+- Fill in the necessary variables to complete the template
+```
+chrome-extension-name: The name to give the Chrome extension
+github-username: The GitHub username whose pull requests will be displayed
+github-org: The GitHub organization that owns the repositories to search for pull requests in
+github-token: Your personal access token that you created above
+```
+- Go to the extensions page in Chrome: chrome://extensions
+- Enable the "Developer mode" checkbox in the top right
+- Click "Load unpacked extension..."
+- Navigate to your project directory, <your-project-name>, which was created above, and click "Select"
+
+## Customizing
+
+You can add links to resources you find useful in `tab.html`. This template provides five `section`s. Each `section` has a header and a single empty list item. Add links as list items to each section and refresh the new tab page to see the changes. You should not need to reinstall the Chrome extension when modifying `tab.html` or `tab.js`
---
+++ template/tab.js
@@ -0,0 +1,130 @@
+function updateClock(){
+  var currentTime = new Date(),
+    currentHours = currentTime.getHours(),
+    currentMinutes = ('0'+currentTime.getMinutes()).slice(-2);
+  document.getElementById("clock").innerHTML = currentHours + ':' + currentMinutes;
+}
+
+updateClock();
+var intervalID = window.setInterval(updateClock, 10000);
+
+function ajax(requestType, requestUrl, data, headers, successCb) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 ) {
+      if (xmlhttp.status == 200 && typeof successCb == 'function')
+        successCb(xmlhttp);
+      else
+        console.log('Error: '+xmlhttp.status);
+    }
+  }
+  xmlhttp.open(requestType, requestUrl, true);
+  for (key in headers)
+    xmlhttp.setRequestHeader(key, headers[key]);
+  if (data) {
+    var dataToSend = [];
+    if (typeof data == 'object') {
+      for (var i in data)
+        dataToSend.push(i+'='+encodeURIComponent(data[i]));
+      dataToSend = dataToSend.join('&');
+    }
+    else
+      dataToSend = data;
+    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xmlhttp.send(dataToSend);
+  }
+  else
+    xmlhttp.send();
+}
+
+(function() {
+  var heights = [],
+    sections = document.querySelectorAll('section');
+  for (var i = 0; i < sections.length; i++)
+    heights.push(sections[i].clientHeight);
+  var maxHeight = Math.max.apply(null, heights);
+  document.getElementById('wrap').style.height = maxHeight+'px';
+
+  chrome.bookmarks.getTree(function(results) {
+    results[0].children.forEach(function(dir) {
+      if (dir.title && dir.title == 'Bookmarks Bar') {
+        var html = '';
+        dir.children.forEach(function(bookmark) {
+          html += '<li><a href="'+bookmark.url+'">'+bookmark.title+'</a></li>';
+        });
+        document.getElementById('bookmarks').innerHTML = html;
+        return;
+      }
+    });
+  });
+
+  var doneRequests = {}, pulls = [];
+
+  var showPulls = function(skipCheck) {
+    if (!skipCheck) {
+      for (var repo in doneRequests)
+        if (!doneRequests[repo])
+          return;
+    }
+    if (!localStorage.getItem('prsMarkup') || Date.now() - 7200000 > localStorage.getItem('prsTimeChecked')) {
+      localStorage.setItem('prsTimeChecked', Date.now());
+      localStorage.setItem('prsMarkup', pulls.join(''));
+      pulls = pulls.join('');
+    } else
+      pulls = localStorage.getItem('prsMarkup');
+    document.getElementById('pull-requests').innerHTML = pulls;
+  };
+
+  var parsePulls = function(r, type) {
+    if (doneRequests[type])
+      return;
+    doneRequests[type] = true;
+    var result = JSON.parse(r.response), html = '';
+    for (var i = 0; i < result.length; i++) {
+      var pull = result[i];
+      if (pull.state == 'open' && pull.user.login == '{{github-username}}') {
+        if (html == '')
+          html = '<ul><h2><a href="https://github.com/{{github-org}}/'+type+'">'+type+'</a></h2>';
+        html += '<li><a href="'+pull.html_url+'?w=1">'+pull.title+'</a>:&nbsp;&nbsp;<code>'+pull.head.ref+'</code></li>';
+      }
+    }
+    html += (html == '' ? '' : '</ul>');
+    pulls.push(html);
+    showPulls();
+  };
+
+  var lastRepoCount = 0,
+    allRepos = [];
+
+  var parseRepos = function() {
+    allRepos.forEach(function(repoName) {
+      ajax('GET', 'https://api.github.com/repos/{{github-org}}/'+repoName+'/pulls', false, {Authorization: 'token {{github-oauth-token}}'}, function(r) {
+        parsePulls(r, repoName);
+      });
+    });
+  };
+
+  var getAllPrs = function(page) {
+    if (Date.now() - 7200000 <= localStorage.getItem('prsTimeChecked'))
+      return showPulls(true);
+    if (lastRepoCount > 0 && lastRepoCount % 100 !== 0)
+      return parseRepos();
+    ajax('GET', 'https://api.github.com/orgs/{{github-org}}/repos?per_page=100&page='+page, false, {Authorization: 'token {{github-oauth-token}}'}, function(r) {
+      var repos = JSON.parse(r.response);
+      lastRepoCount = repos.length;
+      repos.forEach(function(repo) {
+        doneRequests[repo.name] = false;
+        allRepos.push(repo.name);
+      });
+      getAllPrs(page + 1);
+    });
+  };
+
+  getAllPrs(1);
+
+  document.getElementById('refresh-prs-btn').onclick = function() {
+    localStorage.setItem('prsTimeChecked', 0);
+    localStorage.setItem('prsMarkup', '');
+    window.location.reload();
+  };
+}());
